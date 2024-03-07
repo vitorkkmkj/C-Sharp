8d7b744105fa0d90c4c4fc6da48172d3f32b88c1
vitor <155989829+vitorkkmkj@users.noreply.github.com>
2024-03-07 17:28:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+class PP
+{
+    static void Main()
+    {
+        string nome;
+
+        Console.Write("Insira seu nome: ");
+        nome = Console.ReadLine();
+
+
+
+        if (nome == "pedro")
+        {
+            while (nome == "pedro")
+            {
+                string pedro;
+
+                Console.Write("Ola pedro!");
+                break;
+            }
+        }
+        while (nome != "pedro")
+        {
+            string dif;
+            Console.Write("Digite Novamente: ");
+            dif = Console.ReadLine();
+            if (dif == "pedro")
+            {
+                Console.Write("Ola pedro!");
+                break;
+            }
+
+
+
+        }
+    }
+
+}
+
